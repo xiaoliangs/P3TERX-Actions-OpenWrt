02b02a4f483cf000e0e59d36242899ad7323a383
Xiao Liang <xiaoliangdeid@gmail.com>
2026-08-01 13:16:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,25 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+
+
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
+
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
+
+mkdir -p package/custom
+
+# 克隆你的插件到 package/custom 目录
+git clone https://github.com/aopkcn/luci-app-ddns-go package/custom/luci-app-ddns-go
+git clone https://github.com/4IceG/luci-app-sms-tool-js.git package/custom/luci-app-sms-tool-js
+git clone https://github.com/stackia/rtp2httpd.git package/custom/rtp2httpd
+git clone https://github.com/sirpdboy/luci-app-partexp.git package/custom/luci-app-partexp
+git clone --depth 1 https://github.com/vernesong/OpenClash.git package/custom/OpenClash
+git clone --depth 1 https://github.com/cokebar/luci-app-vlmcsd.git package/custom/luci-app-vlmcsd
+git clone https://github.com/sirpdboy/netspeedtest package/custom/netspeedtest
+git clone https://github.com/gdy666/luci-app-lucky.git package/custom/luci-app-lucky
+git clone https://github.com/f8q8/luci-app-autoreboot package/custom/autoreboot
+git clone https://github.com/cokebar/openwrt-vlmcsd.git package/custom/vlmcsd
